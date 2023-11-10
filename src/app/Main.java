396c5efed6118d6fb775e409b9df5f3724a0fd5d
Andrealavi <andrealavino1410.2003@gmail.com>
2023-11-10 18:00:12 +0100

    Created Main class of the app

---
+++ src/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+import views.AppFrame;
+import controllers.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        AppFrame f = new AppFrame("Exams Manager");
+        Controller c = new Controller(f);
+
+        c.setTableModel();
+
+        f.pack();
+    }
+}
