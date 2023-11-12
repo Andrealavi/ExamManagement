8298674b614923d7f37eb58b6df7d0c9ceef3d3e
Andrealavi <andrealavino1410.2003@gmail.com>
2023-11-12 18:50:11 +0100

    Added a new constructor, setPartialExamsInfo method. Changed the data structure for storing partial exams data and the way grade is computed

--- src/models/exam/ComposedExam.java
+++ src/models/exam/ComposedExam.java
@@ -1,13 +1,17 @@
 package models.exam;
 
-public class ComposedExam extends AbstractExam {
-    private Integer[] grades;
-    private Float[] weights;
+import java.util.ArrayList;
 
-    public ComposedExam(String firstName, String lastName, String courseName, Integer[] grades, Float[] weights,
+public class ComposedExam extends AbstractExam {
+    private ArrayList<Integer> grades;
+    private ArrayList<Float> weights;
+    private ArrayList<Boolean> honors;
+
+    public ComposedExam(String firstName, String lastName, String className, ArrayList<Integer> grades,
+            ArrayList<Float> weights,
             Integer credits,
-            boolean honors) {
-        super(firstName, lastName, courseName, credits, honors);
+            ArrayList<Boolean> honors) {
+        super(firstName, lastName, className, credits);
 
         this.grades = grades;
         this.weights = weights;
@@ -15,23 +19,49 @@ public class ComposedExam extends AbstractExam {
         computeGrade();
     }
 
-    private void computeGrade() {
-        int sum = 0;
+    public ComposedExam(String firstName, String lastName, String className, String credits) {
+        super(firstName, lastName, className, credits);
+    }
 
-        for (int i = 0; i < grades.length; i++) {
-            sum += grades[i] * weights[i];
+    private void computeGrade() {
+        float sum = 0.0f;
+        int honorSum = 0;
+
+        for (int i = 0; i < grades.size(); i++) {
+            sum += grades.get(i) * weights.get(i);
+
+            honorSum += honors.get(i) ? 1 : 0;
         }
 
-        grade = (Integer) (sum / grades.length);
+        grade = (int) sum;
+        honor = grade == 30 && honorSum > (honors.size() / 2) ? true : false;
     }
 
     public Integer getGrade() {
         return grade;
     }
 
+    public void setPartialExamsInfo(String[][] partialExams, Integer examNumbers) {
+        grades = new ArrayList<Integer>();
+        weights = new ArrayList<Float>();
+        honors = new ArrayList<Boolean>();
+
+        for (int i = 0; i < examNumbers; i++) {
+            grades.add(Integer.parseInt(partialExams[i][0]));
+            weights.add(Float.parseFloat(partialExams[i][1]));
+            honors.add(Boolean.parseBoolean(partialExams[i][2]));
+        }
+
+        computeGrade();
+    }
+
+    public String getHonor() {
+        return honor ? "Sì" : "No";
+    }
+
     public String[] toStringArray() {
-        String[] stringArray = { firstName, lastName, courseName, grade.toString(), credits.toString(),
-                honors ? "Si" : "No" };
+        String[] stringArray = { firstName, lastName, className, grade.toString(), credits.toString(),
+                getHonor() };
 
         return stringArray;
     }
