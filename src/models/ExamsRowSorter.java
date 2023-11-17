21a803fad55ece9942e98412163171516e5d742a
Andrealavi <andrealavino1410.2003@gmail.com>
2023-11-17 20:18:44 +0100

    Created the row sorter for the exam table in order to apply the filter

---
+++ src/models/ExamsRowSorter.java
@@ -0,0 +1,27 @@
+package models;
+
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
+
+public class ExamsRowSorter extends TableRowSorter<ExamsTableModel> {
+    public ExamsRowSorter(ExamsTableModel model) {
+        super(model);
+    }
+
+    public void createRowFilter(String filterString) {
+        RowFilter<ExamsTableModel, Object> rf = null;
+
+        try {
+            rf = RowFilter.regexFilter("(?i)" + filterString, 0, 1, 2);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        setRowFilter(rf);
+    }
+
+    public void removeRowFilter() {
+        setRowFilter(null);
+    }
+}
