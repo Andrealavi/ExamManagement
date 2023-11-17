936370b3db11abaa4f742b0f1092c544440a2070
Andrealavi <andrealavino1410.2003@gmail.com>
2023-11-17 20:21:39 +0100

    Created Thread that sutosave table data in a log file

---
+++ src/controllers/AutoSaveThread.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import models.ExamsTableModel;
+
+public class AutoSaveThread extends Thread {
+    public ExamsTableModel model;
+
+    public AutoSaveThread(ExamsTableModel model) {
+        this.model = model;
+    }
+
+    public void run() {
+        ExamIO file = new ExamIO("../documents/.datalog");
+
+        while (true) {
+            try {
+                sleep(120000);
+                file.save(model.getEntries());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
