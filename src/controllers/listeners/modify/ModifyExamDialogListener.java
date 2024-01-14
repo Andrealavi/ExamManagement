0c28f59d533711d02f710dd981fbc66152303068
Andrealavi <andrealavino1410.2003@gmail.com>
2024-01-14 08:43:05 +0100

    Made listener inherit from ExamDialogListener

--- src/controllers/listeners/modify/ModifyExamDialogListener.java
+++ src/controllers/listeners/modify/ModifyExamDialogListener.java
@@ -9,11 +9,8 @@ import java.awt.event.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
 
-import controllers.listeners.filter.ClearFilterListener;
-import controllers.listeners.filter.ShowStatsButtonListener;
-import views.AppFrame;
+import controllers.listeners.ExamDialogListener;
 import views.dialogs.*;
 import models.exam.*;
 import models.ExamsTableModel;
@@ -31,21 +28,11 @@ import models.ExamsTableModel;
  * @see controllers.listeners.filter.ShowStatsButtonListener
  * @see java.awt.event.ActionListener
  */
-public class ModifyExamDialogListener implements ActionListener {
-    /**
-     * Dialog with exam data
-     */
-    private AbstractExamDialog dialog;
-
+public class ModifyExamDialogListener extends ExamDialogListener implements ActionListener {
     /**
      * Exam table model
      */
     private ExamsTableModel model;
-
-    /**
-     * Row index of the entry to modify
-     */
-    private int row;
 
     /**
      * Boolean used to check whether the exam entries are saved or not
@@ -53,9 +40,9 @@ public class ModifyExamDialogListener implements ActionListener {
     private AtomicBoolean isSaved;
 
     /**
-     * Boolean used to check whether the exam entries are filtered or not
+     * Row index of the entry to modify
      */
-    private AtomicBoolean isFiltered;
+    private int row;
 
     /**
      * Instantiates class attributes using all the function arguments
@@ -68,42 +55,11 @@ public class ModifyExamDialogListener implements ActionListener {
      */
     public ModifyExamDialogListener(AbstractExamDialog dialog, ExamsTableModel model, int row,
             AtomicBoolean isSaved, AtomicBoolean isFiltered) {
-        this.dialog = dialog;
+        super(dialog, isFiltered);
+
         this.model = model;
         this.row = row;
         this.isSaved = isSaved;
-        this.isFiltered = isFiltered;
-    }
-
-    /**
-     * Creates a {@link models.exam.SimpleExam} entry using data taken from
-     * {@link controllers.listeners.modify.ModifyExamDialogListener#dialog}
-     * 
-     * @return Simple exam entry
-     * @throws ExamInfoException Exception that is thrown if exam data are invalid
-     */
-    private SimpleExam createSimpleEntry() throws ExamInfoException {
-        String[] data = dialog.getFieldsData();
-        SimpleExam examEntry = new SimpleExam(data[0], data[1], data[2], data[3], data[4]);
-
-        return examEntry;
-    }
-
-    /**
-     * Creates a {@link models.exam.ComposedExam} entry using data taken from
-     * {@link controllers.listeners.add.ModifyExamDialogListener#dialog}
-     * 
-     * @return Composed exam entry
-     * @throws ExamInfoException Exception that is thrown if exam data are invalid
-     */
-    private ComposedExam createComposedEntry() throws ExamInfoException {
-        String[] generalInfo = dialog.getFieldsData();
-
-        ComposedExam examEntry = new ComposedExam(generalInfo[0], generalInfo[1], generalInfo[2], generalInfo[3]);
-        examEntry.setPartialExamsInfo(((ModifyComposedExamDialog) dialog).getExamsData(),
-                ((ModifyComposedExamDialog) dialog).getExamNumber());
-
-        return examEntry;
     }
 
     /**
@@ -114,74 +70,28 @@ public class ModifyExamDialogListener implements ActionListener {
         try {
             AbstractExam examEntry;
 
-            if (dialog.getClass().getSimpleName().equals("ModifySimpleExamDialog")) {
-                examEntry = createSimpleEntry();
+            if (super.getDialog().getClass().getSimpleName().equals("ModifySimpleExamDialog")) {
+                examEntry = createSimpleExamEntry();
             } else {
-                examEntry = createComposedEntry();
+                examEntry = createComposedExamEntry();
             }
 
             model.updateEntryAtRow(examEntry, row);
 
             isSaved.set(false);
 
-            if (isFiltered.get()) {
+            if (super.getFilterBoolean().get()) {
                 updateFilter();
             }
 
-            dialog.dispose();
+            super.getDialog().dispose();
         } catch (ExamInfoException err) {
-            JOptionPane.showMessageDialog(dialog, err.getMessage(), "Error message", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(super.getDialog(), err.getMessage(), "Error message",
+                    JOptionPane.ERROR_MESSAGE);
         } catch (NumberFormatException err) {
-            JOptionPane.showMessageDialog(dialog,
+            JOptionPane.showMessageDialog(super.getDialog(),
                     "Invalid value inserted.\nPlease make sure you have inserted valid number values for grade and credits fields.",
                     "Error message", JOptionPane.ERROR_MESSAGE);
         }
-    }
-
-    /**
-     * Updates the data displayed by the filtered table
-     */
-    private void updateFilter() {
-        AppFrame frame = (AppFrame) dialog.getParent();
-
-        JTable table = frame.getTablePanel().getTable();
-
-        Float gradeSum = 0.0f;
-        Integer creditSum = 0;
-
-        Integer[] gradesFrequencies = new Integer[13];
-
-        for (int i = 0; i < gradesFrequencies.length; i++) {
-            gradesFrequencies[i] = 0;
-        }
-
-        for (int i = 0; i < table.getRowSorter().getViewRowCount(); i++) {
-            gradesFrequencies[Integer
-                    .parseInt(table.getModel().getValueAt(table.convertRowIndexToModel(i), 3).toString()) - 18]++;
-
-            gradeSum += Float
-                    .parseFloat(table.getModel().getValueAt(table.convertRowIndexToModel(i), 3).toString())
-                    * Float.parseFloat(table.getModel().getValueAt(table.convertRowIndexToModel(i), 4).toString());
-
-            creditSum += Integer.parseInt(table.getModel().getValueAt(table.convertRowIndexToModel(i), 4).toString());
-        }
-
-        Integer weightedAverage = (int) (gradeSum / creditSum);
-
-        frame.getFilterPanel().getShowStatsButton()
-                .removeActionListener(frame.getFilterPanel().getShowStatsButton().getActionListeners()[0]);
-        frame.getFilterPanel().getClearFilterButton()
-                .removeActionListener(frame.getFilterPanel().getClearFilterButton().getActionListeners()[0]);
-
-        frame.removeFilterPanel();
-
-        frame.addFilterPanel();
-
-        frame.getFilterPanel().getGradeField().setText(weightedAverage.toString());
-
-        frame.getFilterPanel().getClearFilterButton()
-                .addActionListener(new ClearFilterListener(frame, table, isFiltered));
-        frame.getFilterPanel().getShowStatsButton()
-                .addActionListener(new ShowStatsButtonListener(frame, gradesFrequencies));
     }
 }
